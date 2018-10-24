e18ec659b44f94ce6ac6a71586514c3acc9eb904
Abdel Mabrouk <aek.mabrouk@gmail.com>
2018-10-25 00:49:52 +0100

    Added HelloJavaWorld Program

---
+++ HelloJavaWorld.java
@@ -0,0 +1,13 @@
+/* This is a simple Java program. 
+   FileName : "HelloJavaWorld.java". */
+class HelloJavaWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, Java World"); 
+    } 
+} 
+// To compile use: javac HelloJAvaWorld.java
+// To excute use: java HelloJavaWorld
